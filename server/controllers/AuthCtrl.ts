391db7c919138b36c8e6c2c4cc0793a641c7c548
Gilson Gilbert <gilson.gilbert@bridge-global.com>
2020-10-23 16:18:28 +0530

    feat(register-email): send email on register send email to user and CS on register

--- server/controllers/AuthCtrl.ts
+++ server/controllers/AuthCtrl.ts
@@ -5,7 +5,8 @@ import BaseCtrl from './BaseCtrl';
 import { RequestAuth } from '../models/v1/Auth';
 import { passport, getActiveApps } from '../services/auth';
 import { generateUrl, getDomain } from '../services/inject';
-import { UserModel } from '../models/v1/User';
+import { IUser, UserModel } from '../models/v1/User';
+import { loadEmail, sendMail } from '../services/email';
 
 const path = require('path');
 
@@ -45,6 +46,52 @@ export default class AuthCtrl extends BaseCtrl {
     }
   };
 
+  sendRegisterEmail = async (doc: IUser) => {
+    try {
+      let template = loadEmail('register');
+      template = template.split('%FIRSTNAME%').join(doc.firstName);
+      if (doc.lastName) {
+        template = template.split('%LASTNAME%').join(doc.lastName);
+      } else {
+        template = template.split('%LASTNAME%').join('');
+      }
+      const mailOptionsUser = {
+        id_from: doc._id,
+        from: process.env.EMAIL_FROM,
+        to: doc.email,
+        subject: 'Successfully register to CashStory',
+        text: 'Successfully register to CashStory',
+        html: template,
+      };
+      let templateUs = loadEmail('register-us');
+      templateUs = templateUs.split('%EMAIL%').join(doc.email);
+      templateUs = templateUs.split('%FIRSTNAME%').join(doc.firstName);
+      if (doc.company) {
+        templateUs = templateUs.split('%COMPANYNAME%').join(doc.company.name);
+      } else {
+        templateUs = templateUs.split('%COMPANYNAME%').join('');
+      }
+      templateUs = templateUs.split('%ROLE%').join(doc.userRole);
+      templateUs = templateUs.split('%MANAGER%').join(doc.manager);
+      templateUs = templateUs.split('%MOBILE%').join(doc.phoneNumber);
+      const mailOptionsAdmin = {
+        id_from: doc._id,
+        from: process.env.EMAIL_FROM,
+        to: process.env.EMAIL_ADMIN,
+        subject: `New User: ${doc.email}`,
+        text: 'New User',
+        html: templateUs,
+      };
+      return Promise.all([
+        sendMail(mailOptionsUser),
+        sendMail(mailOptionsAdmin),
+      ]);
+    } catch (err) {
+      console.error('email error', err);
+      return Promise.resolve();
+    }
+  };
+
   register = async (req, res: Response): Promise<any> => {
     const UserMod = UserModel();
     const userFound = await UserMod.findOne({ email: req.body.email });
@@ -54,7 +101,9 @@ export default class AuthCtrl extends BaseCtrl {
         password: req.body.password,
         firstName: req.body.full_name,
       };
-      await new UserMod(userData).save();
+      const newUser = await new UserMod(userData).save();
+      const jsonDoc = newUser.toJSON();
+      await this.sendRegisterEmail(jsonDoc);
       return res.status(200).json(userData);
     }
     return res.status(500).json({ message: 'Email already registred' });
